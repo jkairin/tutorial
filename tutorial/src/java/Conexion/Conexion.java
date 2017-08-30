335e24b8df703a4872cd800655d0c22cd1d4459c
mscobar <31392858+mscobar@users.noreply.github.com>
2017-08-30 15:27:38 -0300

    otra modificacion

---
+++ tutorial/src/java/Conexion/Conexion.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Conexion {
+    
+    String cedula, nombre, apellido, telefono, direccion;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+}
--- tutorial/web/nuevo.jsp
+++ tutorial/web/nuevo.jsp
@@ -12,6 +12,6 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hola mundo html!</h1>        
+        <h1>Hola mundo JSP</h1>        
     </body>
 </html>
